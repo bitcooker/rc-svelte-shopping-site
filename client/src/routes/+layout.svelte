83896b86d0c569e220d1eb54c741c0c00a63fc85
bitcooker <dreamhighpassionhigh@gmail.com>
2023-02-22 14:54:53 +0000

    Init theme and routes

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -2,4 +2,10 @@
   import "../app.css";
 </script>
 
+<a href="/"><h1 class="font-bold">Logo</h1></a>
+
+<nav>
+    <a href="/about">About</a>
+</nav>
+
 <slot />
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1 class="font-bold">Welcome to K-Mart</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-screen">
+    <h1 class="font-bold">Welcome to K-Mart</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
